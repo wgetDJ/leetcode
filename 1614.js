9557dcc171bf26d0f5bc6c90b7ae268bbdf3829c
Dibyajyoti Mishra <dibyajyotimishra@NHD050535.local>
2024-04-01 21:40:26 +0530

    ADD: 1614

---
+++ 1614.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+
+var maxDepth = function(s) {
+    let res = 0
+    let count = 0
+    let depth = 0
+
+    for (ele of s) {
+        if(ele === "(") {
+            count++
+        }
+        if(count > 0 && count > depth) {
+            depth++
+        }
+        if(ele === ")") {
+            count--
+        }
+        if(count === 0 && res < depth) {
+            res = depth
+        }
+    }
+    return res
+};
